aecfd5d97a4ebfa4cbd0058315d98f726777ef22
Marko Ilic <ivanstojanovic00@gmail.com>
2023-02-28 14:26:34 +0100

    breakthroughs with animations, first with the rules and with the tips

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
+//landing page logic 
 let text = document.querySelector('.text')
 const h2 = document.querySelector('h2')
 const rulesTitle = document.querySelector('.rules-heading')
 let tip = document.querySelector('.tip')
 const disposableElements = Array.from(document.getElementsByClassName('future-removal'))
-let quizLayout = document.querySelector('.game-layout')
 const buttonShort = document.querySelector('.shortGame')
 const buttonLong = document.querySelector('.longGame')
 
@@ -25,25 +25,20 @@ const fadeLeftAnimation = [
 
 buttonShort.addEventListener('click', () => {
     disposableElements.forEach((el, i) => {
-    setTimeout(() => {
-        el.animate(fadeLeftAnimation, 1000)
-        //elementRemoval(el)
+        setTimeout(() => {
+            el.animate(fadeLeftAnimation, 1000)
+            //elementRemoval(el)
         }, i * 30);
     });
     setTimeout(elementRemoval, 1000, text)
     setTimeout(quizStart, 1005, quizLayout)
 })  
 
-
-
-
-
-
-
- /* Things to do:
- 1. Use keyframe formats to animate rules element removal and arrival of quiz section
- 2. Start copying/writing the api fetch logic and the quiz logic
- 3. try to make the code with Meg a bit more readable and easier if you can
+let quizLayout = document.querySelector('.game-layout')
+/* Things to do:
+1. Use keyframe formats to animate rules element removal and arrival of quiz section
+2. Start copying/writing the api fetch logic and the quiz logic
+3. try to make the code with Meg a bit more readable and easier if you can
  4. break it down into smaller functions if possible
  5. do DSA!!!!!!!!!!!!!!!!!!!!!
  */
